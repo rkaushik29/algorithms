ae67a226df076c36ba1539602f078b5943e1c537
rkaushik29 <rohit.rk.rk1@gmail.com>
2021-08-18 11:23:08 -0400

    rotated bin search

---
+++ binsearch/rotatedsortedsearch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0; int end = nums.size() - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            
+            if (target == nums[mid]) return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target < nums[mid] && target >= nums[start])
+                    end = mid - 1;
+                else start = mid + 1;
+            }
+            else {
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else 
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
