54d3802353f1a05f1208a73b7688e98a574f48e1
r3kaushi <r3kaushi@uwaterloo.ca>
2023-10-08 16:12:00 -0400

    interface

--- arrays/swap.go
+++ arrays/swap.go
@@ -3,10 +3,19 @@ import "fmt"
 
 const a = "100"
 
+type test interface {
+	swap()
+	area() int
+}
+
 func swap(sw []int) {
 	for a,b := 0, len(sw)-1; a < b ; a,b = a+1, b-1 {
 		sw[a], sw[b] = sw[b], sw[a]
 	}
+}
+
+func area(a int, b int) (int) {
+	return a * b
 }
 
 func main() {
